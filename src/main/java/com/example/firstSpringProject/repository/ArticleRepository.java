bd72eb56eb9f8b1b1cccf99d671859f06332722e
JinsuNa <shs1401009@gmail.com>
2025-02-12 19:16:14 +0900

    1. dto - entity - repository 까지 옮기는 작업 완료.

--- src/main/java/com/example/firstSpringProject/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.firstSpringProject.repository;
-
-import com.example.firstSpringProject.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-
-}
